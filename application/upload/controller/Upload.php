ac61782b9ce4b071771196b6c9f8e80909e83e0b
unknown <shenlidong@qdairlines.com>
2018-06-15 09:18:42 +0800

    提交文件

--- application/upload/controller/Upload.php
+++ application/upload/controller/Upload.php
@@ -6,9 +6,11 @@ use think\Session;
 class Upload extends ControllerBase
 {
     public function up(){
+        $path = ROOT_PATH.'/public/';
+//        print_r(ROOT_PATH  );exit;
         if(is_uploaded_file($_FILES['excel_file']['tmp_name'])){                                            //判断是否是上传文件
             //unlink($_FILES['file1']['tmp_name']);
-            move_uploaded_file($_FILES['excel_file']['tmp_name'], "./{$_FILES['excel_file']['name']}");     //将缓存文件移动到指定位置
+            move_uploaded_file($_FILES['excel_file']['tmp_name'], $path."{$_FILES['excel_file']['name']}");     //将缓存文件移动到指定位置
         }
     }
 
