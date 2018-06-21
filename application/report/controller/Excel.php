3de895de0cb7b8466497638067d25bbed3873071
unknown <shenlidong@qdairlines.com>
2018-06-21 17:08:35 +0800

    提交代码

--- application/report/controller/Excel.php
+++ application/report/controller/Excel.php
@@ -6,9 +6,9 @@ vendor("PHPExcel.PHPExcel");
 
 class Excel extends ControllerBase
 {
-    public function liantong_report(){
+    protected function im_report($filename,$sheets){
 //        $file = iconv("utf-8", "gb2312", $file);   //转码
-        $file = ROOT_PATH.'public/upload/1_liantong2.xlsx';
+        $file = ROOT_PATH.'public/upload/'.$filename;
         if(empty($file) OR !file_exists($file)) {
             die('file not exists!');
         }
@@ -35,27 +35,38 @@ class Excel extends ControllerBase
             $objExcel = $PHPReader->load($file);
         }
 
-        $sheet = $objExcel ->getSheet(0);
-        $cellName = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ');
-
-        $columnH = $sheet->getHighestColumn();   //取得最大的列号
-        $columnCnt = array_search($columnH, $cellName);
-        $rowCnt = $sheet->getHighestRow();   //获取总行数
-
         $data = array();
-        for($_row=1; $_row<=$rowCnt; $_row++){  //读取内容
-            for($_column=0; $_column<=$columnCnt; $_column++){
-                $cellId = $cellName[$_column].$_row;
-                $cellValue = $sheet->getCell($cellId)->getValue();
-                //$cellValue = $currSheet->getCell($cellId)->getCalculatedValue();  #获取公式计算的值
-                if($cellValue instanceof \PHPExcel_RichText){   //富文本转换字符串
-                    $cellValue = $cellValue->__toString();
-                }
+        $sheet_cnt = explode(',',$sheets);
 
-                $data[$_row][$cellName[$_column]] = $cellValue;
+        foreach ($sheet_cnt as $k => $v) {
+            $sheet = $objExcel ->getSheet($k);
+            $cellName = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ');
+
+            $columnH = $sheet->getHighestColumn();   //取得最大的列号
+            $columnCnt = array_search($columnH, $cellName);
+            $rowCnt = $sheet->getHighestRow();   //获取总行数
+
+            for($_row=1; $_row<=$rowCnt; $_row++){  //读取内容
+                for($_column=0; $_column<=$columnCnt; $_column++){
+                    $cellId = $cellName[$_column].$_row;
+                    $cellValue = $sheet->getCell($cellId)->getValue();
+                    //$cellValue = $currSheet->getCell($cellId)->getCalculatedValue();  #获取公式计算的值
+                    if($cellValue instanceof \PHPExcel_RichText){   //富文本转换字符串
+                        $cellValue = $cellValue->__toString();
+                    }
+
+                    $data[$k][$_row][$cellName[$_column]] = $cellValue;
+                }
             }
         }
-        print_r($data);exit;
+
         return $data;
+    }
+
+    public function import_phoneuser(){
+        $filename = 'baoxiaojieguo.xlsx';
+        $sheet = '0';
+        $data = $this->im_report($filename,$sheet);
+        print_r($data);exit;
     }
 }
